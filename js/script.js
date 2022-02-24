f364c6bad32e230aa2ab3056174a24e63d83d0f0
pragati dubey <pragati@techalchemy.co>
2022-02-24 21:50:54 +0530

    added-files

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(".sidebar-dropdown > a").click(function() {
+  $(".sidebar-submenu").slideUp(200);
+  if (
+    $(this)
+      .parent()
+      .hasClass("active")
+  ) {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .parent()
+      .removeClass("active");
+  } else {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .next(".sidebar-submenu")
+      .slideDown(200);
+    $(this)
+      .parent()
+      .addClass("active");
+  }
+});
+
+$("#close-sidebar").click(function() {
+  $(".page-wrapper").removeClass("toggled");
+});
+$("#show-sidebar").click(function() {
+  $(".page-wrapper").addClass("toggled");
+});
+
+    $(document).ready( function () {
+    $('#myTable').DataTable();
+} );
+$(document).ready( function () {
+  $('#myTablee').DataTable();
+} );
+$(document).ready( function () {
+  $('#delivered').DataTable();
+} );
+ 
